272a771d18fe49cbec81b78d5e278c1a57e337ee
Rik de Groot <hwdegroot@gmail.com>
2025-05-07 11:37:54 +0200

    chore: Use Mistral remote api

--- lua/plugins/gen.nvim.lua
+++ lua/plugins/gen.nvim.lua
@@ -89,7 +89,8 @@ return {
     quit_map = "q", -- set keymap to close the response window
     retry_map = "<c-r>", -- set keymap to re-send the current prompt
     accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
-    host = "localhost", -- The host running the Ollama service.
+    protocol = "https",
+    host = "api.mistral.ai", -- The host running the Ollama service.
     port = "11434", -- The port on which the Ollama service is listening.
     display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
     show_prompt = false, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
@@ -97,11 +98,21 @@ return {
     no_auto_close = false, -- Never closes the window automatically.
     file = false, -- Write the payload to a temporary file to keep the command short.
     hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
-    init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+    init = function() end,
     -- Function to initialize Ollama
+    -- function(options)
+    --   pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+    --   end,
     command = function(options)
+      local api_key = os.getenv("MISTRALAI_API_KEY")
       local body = {model = options.model, stream = true}
-      return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
+      return "curl " ..
+        "--silent " ..
+        "--no-buffer " ..
+        --"-X POST " ..
+        "--header 'Authorization: Bearer " .. api_key .. "' " ..
+        "--location " .. options.protocol .. "://" .. options.host .. ":" .. options.port .. "/api/chat " ..
+        "--data '" .. body .. "'"
     end,
     -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
     -- This can also be a command string.
